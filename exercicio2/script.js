09d93c270d4f8aa9de2a2060976a5dcdda2ac049
Marco_Borges <mas.borges78@gmail.com>
2022-08-04 20:53:17 -0300

    Exercícios feitos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,37 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 18  
+let terninouEnsinoMedio = true
+let isCursandoOutraFaculdade = true
 
-if(){
-
+if(idade>=18){
+    console.log('Você é maior de idade!')
 }else{
-	
+    console.log('Você é menor de idade!')
 }
+if(terninouEnsinoMedio){
+    console.log('Você terminou o ensino médio.')
+}else{
+    console.log('Não terminou o ensino médio.')
+}
+if(isCursandoOutraFaculdade === false){
+    console.log('Não está cursando faculdade.')
+}else{
+    console.log('Esta cursando faculdade')
+}
+
+/* Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
+
+Crie um if para cada variável, checando as seguintes afirmacões:
+
+Se a pessoa tem 18 anos ou mais;
+Se a pessoa terminou o ensino médio;
+Se a pessoa NÃO está cursando alguma faculdade;
+Crie um if que imprima que a afirmacão é verdadeira, e um else para imprimir que a afirmacão não é verdadeira.
+
+Exemplo
+Caso a primeira pessoa tenha idade >=18:
+
+"A pessoa é maior de idade"
+
+e caso não seja:
+
+"A pessoa é menor de idade" */
